0e7d1242c0bea65cb7b23ed960c2c6a46251800a
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-11-25 11:19:35 +0100

    Fixed #14

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,9 @@
 const header = document.querySelector('header')
 const menuIcon = document.querySelector('.menu-icon')
+const menuTitle = document.querySelector('.menu-title')
 
 
 menuIcon.addEventListener('click', function() {
     header.classList.toggle('open')
-    console.log('clicked')
+    menuTitle.textContent = "Close menu"
 })
